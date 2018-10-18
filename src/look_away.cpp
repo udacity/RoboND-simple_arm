673f17d4e36d407b4a9484caf221969ae75e4552
Karim Chamaa <karim@udacity.com>
2018-10-18 11:02:42 -0700

    sub1

--- src/look_away.cpp
+++ src/look_away.cpp
@@ -70,7 +70,7 @@ int main(int argc, char** argv)
     client = n.serviceClient<simple_arm::GoToPosition>("/arm_mover/safe_move");
 
     // Subscribe to /simple_arm/joint_states topic to read the arm joints position inside the joint_states_callback function
-    ros::Subscriber sub = n.subscribe("/simple_arm/joint_states", 10, joint_states_callback);
+    ros::Subscriber sub1 = n.subscribe("/simple_arm/joint_states", 10, joint_states_callback);
 
     // Subscribe to rgb_camera/image_raw topic to read the image data inside the look_away_callback function
     ros::Subscriber sub2 = n.subscribe("rgb_camera/image_raw", 10, look_away_callback);
